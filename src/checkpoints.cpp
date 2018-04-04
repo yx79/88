42bdab8a92de1a7bfeaf865247d12a6679bc1b9b
yx79 <yx79@georgetown.edu>
2018-04-04 23:04:31 +0800

    Update checkpoints.cpp

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -28,18 +28,6 @@ namespace Checkpoints
     static MapCheckpoints mapCheckpoints =
         boost::assign::map_list_of
         (0,	Params().HashGenesisBlock() )
-		(11, uint256("0xe888335045b9a26f0e5c23d397531bdf0675c2e82ee770a0f8a8df07b48b242a"))
-		(26, uint256("0xa1d3531bfe1c0a057f5218ddec9459f5adb214f5c4ac1dcf6fc4dbe71fec1d4a"))
-		(42, uint256("0xe09bc55c77d399a647143c6581e7e9581ef0cde4333af5b73c848b6bdc556524"))
-		(49, uint256("0xf74b1c9c869e8f647163d6de5a76870ccf19027bccb2e13de8bae71e9deabf5b"))
-		(527, uint256("0x0444f8c42866530a0ff5bfb81b9fb57ac847eaea71ae6081bbdc80402e07a373"))
-		(1548, uint256("0x8150c0deaa6ba6562f968c712698bbcfefb2e621a41c9b66d02c6b84898636dd"))
-		(2192, uint256("0xca69d14c613732e23f9716b3bbafc3fd9aaba7df3bce634d6a35d155a79f1ac3"))
-		(2620, uint256("0x19abcbb1acd7d4d09861cc156139210463206b270bf82c0b838e7dd639029d58"))
-		(2621, uint256("0x69cec274d07108577ddeaab3ba57aa94091f85b6741c3915edec0ef7bc7c61ff"))
-		(2622, uint256("0xc68061fa2a4f83574bd2908eb3485d4e7bcfd662df641c66626d86448c300176"))
-		(3082, uint256("0xd7464b2009ecbcfc82240856aa23cc5d6f51d1179445f5465ebafa512d33b18e"))
-		
     ;
 
     // TestNet has no checkpoints
