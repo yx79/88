c8de0b77e170fdf6ea70e30939886954efc9f5b3
yx79 <yx79@georgetown.edu>
2018-04-09 20:43:20 +0800

    Update ecwrapper.cpp

--- src/ecwrapper.cpp
+++ src/ecwrapper.cpp
@@ -154,12 +154,11 @@ void CECKey::GetSecretBytes(unsigned char vch[32]) const {
 void CECKey::SetSecretBytes(const unsigned char vch[32]) {
     bool ret;
     BIGNUM *bn;
-    BN_init(bn.bn);
-    ret = BN_bin2bn(vch, 32, &bn) != NULL;
+    ret = BN_bin2bn(vch, 32, bn.bn) != NULL;
     assert(ret);
-    ret = EC_KEY_regenerate_key(pkey, &bn) != 0;
+    ret = EC_KEY_regenerate_key(pkey, bn.bn) != 0;
     assert(ret);
-    BN_clear_free(&bn);
+    BN_clear_free(bn.bn);
 }
 
 int CECKey::GetPrivKeySize(bool fCompressed) {
