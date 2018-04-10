e750e8cf560d89d933aa9a1138f31921015f5d53
yx79 <yx79@georgetown.edu>
2018-04-11 01:22:53 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ cd ../..
 cd src/secp256k1
 export PATH=/mnt/mxe/usr/bin:$PATH
 sudo ./autogen.sh
-sudo ./configure --host=i686-w64-mingw32.static --with-bignum=no --enable-module-recovery use_tests=yes --with-field=32bit --with-scalar=32bit --with-asm=x86_64
+sudo ./configure --host=i686-w64-mingw32.static --with-bignum=no --enable-module-recovery use_tests=no cross_compiling=yes --with-field=32bit --with-scalar=32bit --with-asm=x86_64
  sudo TARGET_OS=OS_WINDOWS_CROSSCOMPILE make install CC=/mnt/mxe/usr/bin/i686-w64-mingw32.static-g++ CXX=/mnt/mxe/usr/bin/i686-w64-mingw32.static-g++ libsecp256k1.la libsecp256k1.so
 sudo make install
 cd ../..
