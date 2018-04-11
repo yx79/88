e5a88be5223fab78c742d670b6c5a9b9e70f24f6
yx79 <yx79@georgetown.edu>
2018-04-11 22:55:41 +0800

    Update sendmessagesdialog.cpp

--- src/qt/sendmessagesdialog.cpp
+++ src/qt/sendmessagesdialog.cpp
@@ -34,7 +34,7 @@ SendMessagesDialog::SendMessagesDialog(Mode mode, Type type, QWidget *parent) :
 #if QT_VERSION >= 0x040700
      /* Do not move this to the XML file, Qt before 4.7 will choke on it */
     if(mode == SendMessagesDialog::Encrypted)
-        ui->addressFrom->setPlaceholderText(tr("Enter a EighthCoin address (e.g. D8gZqgY4r2RoEdqYk3QsAqFckyf9pRHN6i)"));
+        ui->addressFrom->setPlaceholderText(tr("Enter a EighthCoin address (e.g. 8PHhbaTedKYp9MBdJswyTngF3ziaWMiygT)"));
  #endif
     addEntry();
 
