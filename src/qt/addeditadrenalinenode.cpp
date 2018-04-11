30d484d19718e2a34aba1560dc5b086e9bd9ac34
eight <888@gmail.com>
2018-04-11 13:20:41 +0000

    windows chagne folder name

--- src/qt/addeditadrenalinenode.cpp
+++ src/qt/addeditadrenalinenode.cpp
@@ -28,7 +28,7 @@ AddEditAdrenalineNode::AddEditAdrenalineNode(QWidget *parent) :
     ui->aliasLineEdit->setPlaceholderText("Enter your Masternode alias");
     ui->addressLineEdit->setPlaceholderText("Enter your IP & port");
     ui->privkeyLineEdit->setPlaceholderText("Enter your Masternode private key");
-    ui->txhashLineEdit->setPlaceholderText("Enter your 5000 8TH TXID");
+    ui->txhashLineEdit->setPlaceholderText("Enter your 800,000 8TH TXID");
     ui->outputindexLineEdit->setPlaceholderText("Enter your transaction output index");
     ui->donationaddressLineEdit->setPlaceholderText("Enter a 8TH address for your MN donation");
     ui->donationpercentageLineEdit->setPlaceholderText("Input the % for the donation");
--- src/util.cpp
+++ src/util.cpp
@@ -1077,7 +1077,7 @@ boost::filesystem::path GetDefaultDataDir()
     // Unix: ~/.EighthCoin
 #ifdef WIN32
     // Windows
-    return GetSpecialFolderPath(CSIDL_APPDATA) / "EighthCoin";
+    return GetSpecialFolderPath(CSIDL_APPDATA) / "EighthCoin8";
 #else
     fs::path pathRet;
     char* pszHome = getenv("HOME");
@@ -1089,10 +1089,10 @@ boost::filesystem::path GetDefaultDataDir()
     // Mac
     pathRet /= "Library/Application Support";
     fs::create_directory(pathRet);
-    return pathRet / "EighthCoin";
+    return pathRet / "EighthCoin8";
 #else
     // Unix
-    return pathRet / ".EighthCoin";
+    return pathRet / ".EighthCoin8";
 #endif
 #endif
 }
